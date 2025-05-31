f3bd44e1fc36752a6e7a4e9a73c11a903c23a1c8
Im-4hmad <ahmadreza.y85@gmail.com>
2025-05-31 20:19:19 +0330

    Add person class

--- obj/Debug/net8.0/DormProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/DormProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DormProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30cf02436f0fb0d5cc9248727453f81155b2e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DormProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DormProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
